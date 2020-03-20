ba4ac1dd363bcd9c0b48fe56527a29e32b564f69
Patrick Furtak <patrick@patrickfurtak.com>
2020-03-20 14:05:16 -0400

    added in search route, added in search options from loop, added in search page html

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Listing
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from listings.choices import price_choices, bedroom_choices, state_choices
 
 
 def index(request):
@@ -12,6 +13,7 @@ def index(request):
     
     context = {
         'listings': paged_listings
+    
     }
 
     return render(request, 'listings/listings.html', context)
@@ -26,4 +28,11 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
+    }
+
+    return render(request, 'listings/search.html', context)
