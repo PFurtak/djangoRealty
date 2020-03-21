85e30b06790f6029e1ce3a03cf5e19ad3d2bdc24
Patrick Furtak <patrick@patrickfurtak.com>
2020-03-21 16:12:37 -0400

    inquiry form posts to DB, alert messages display properly

--- contacts/views.py
+++ contacts/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Contact
 
 def contact(request):
-    return
+    if request.method == 'POST':
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        name = request.POST['name']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+        seller_email = request.POST['seller_email']
+        
+        contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
+
+        contact.save()
+
+        messages.success(request, 'Thank you, we will reach out to you shortly.')
+        return redirect('/listings/'+listing_id)
