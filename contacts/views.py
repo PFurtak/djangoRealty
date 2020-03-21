234e21d184d878c343de2ab769f0804549078e50
Patrick Furtak <patrick@patrickfurtak.com>
2020-03-21 18:07:16 -0400

    added in email alert on inquiry, currently commented out

--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Contact
+from django.core.mail import send_mail
 
 def contact(request):
     if request.method == 'POST':
@@ -13,9 +14,26 @@ def contact(request):
         user_id = request.POST['user_id']
         seller_email = request.POST['seller_email']
         
+        if request.user.is_authenticated:
+            user_id = request.user.id 
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(request, 'You have already made an inquiry on this listing')
+                return redirect('/listings/'+listing_id)
+
         contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
 
         contact.save()
+
+        # #mailing
+        # send_mail(
+        #     'Property inquiry',
+        #     'There has been an inquiry on ' + listing + '. Sign in for more details.',
+        #     'dummyemail@fake.com',
+        #     [seller_email, 'dummy2@gmail.com', 'admin@djangorealty.com'],
+        #     fail_silently=False
+        # )
+
 
         messages.success(request, 'Thank you, we will reach out to you shortly.')
         return redirect('/listings/'+listing_id)
--- djangoRealty/settings.py
+++ djangoRealty/settings.py
@@ -143,3 +143,11 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
     
 }
+
+# # EMAIL
+
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD=''
+# EMAIL_USE_TLS=True
