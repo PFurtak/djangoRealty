df47ba59256ad6365db9a58dfb5f441469ae4860
Patrick Furtak <patrick@patrickfurtak.com>
2020-03-21 00:18:25 -0400

    setup login functionality

--- accounts/views.py
+++ accounts/views.py
@@ -12,9 +12,11 @@ def register(request):
      password2 = request.POST['password2']
 
      if password == password2:
+
         if User.objects.filter(username=username).exists():
             messages.error(request, 'Username is already in use')
             return redirect('register')
+
         else:
            if User.objects.filter(email=email).exists():
             messages.error(request, 'Email address is already in use')
@@ -27,22 +29,36 @@ def register(request):
                 messages.success(request, 'please login')
                 return redirect('login')
 
-           
-
      else: 
       messages.error(request, 'Passwords do not match')
       return redirect('register')
+
     else:
      return render(request, 'accounts/register.html')
 
+
 def login(request):
     if request.method == 'POST':
-     return
+     username = request.POST['username']
+     password = request.POST['password']
+     user = auth.authenticate(username=username, password=password)
+
+     if user is not None:
+         auth.login(request, user)
+         messages.success(request, 'You are now logged in')
+         return redirect('dashboard')
+
+     else:
+        messages.error(request, 'Invalid credentials')
+        return redirect('login')
+
     else:
       return render(request, 'accounts/login.html')
 
+
 def logout(request):
     return redirect('index')
+
 
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
